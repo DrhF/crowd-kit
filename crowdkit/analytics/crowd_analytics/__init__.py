37a11e11310ed20ab4309ce00e17991e9a3a6623
DrhF <mr.fedulow@gmail.com>
2022-07-19 11:11:23 +0300

    Add existing methods under common interface

---
+++ crowdkit/analytics/crowd_analytics/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+    'ConsistencyCalculator',
+    'KrippendorffAlphaCalculator',
+    'UncertaintyCalculator'
+]
+
+from .consistency import ConsistencyCalculator
+from .krippendorff_alpha import KrippendorffAlphaCalculator
+from .uncertainty import UncertaintyCalculator
